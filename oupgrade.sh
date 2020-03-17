c89de25d3df348861ad6812ba279649b8b6a0594
greenbreakfast <lazardemin@gmail.com>
2020-03-17 17:30:59 -0400

    isolated code that checks version and build numbers into functions, directly including json library, whitespace fixes

--- oupgrade.sh
+++ oupgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# include the Onion sh lib
-. /usr/lib/onion/lib.sh
+# include the json sh library
+. /usr/share/libubox/jshn.sh
 
 # setup variables
 bUsage=0
@@ -13,6 +13,7 @@ bUpgrade=0
 bBuildMismatch=0
 bJsonOutput=0
 bCheckOnly=0
+bDebug=0
 
 deviceVersion=""
 deviceVersionMajor=""
@@ -48,20 +49,20 @@ timeout=500
 
 # function to print script usage
 Usage () {
-    echo "Functionality:"
-    echo "  Check if new Onion firmware is available and perform upgrade"
-    echo ""
-    echo "Usage: $0"
-    echo ""
-    echo "Arguments:"
-    echo " -h, --help        Print this usage prompt"
-    echo " -v, --version     Just print the current firmware version"
-    echo " -l, --latest      Use latest repo version (instead of stable version)"
-    echo " -f, --force       Force the upgrade, regardless of versions"
-    echo " -c, --check       Only compare versions, do not actually update"
-    echo " -u, --ubus        Script outputs only json"
+		echo "Functionality:"
+		echo "  Check if new Onion firmware is available and perform upgrade"
+		echo ""
+		echo "Usage: $0"
+		echo ""
+		echo "Arguments:"
+		echo " -h, --help        Print this usage prompt"
+		echo " -v, --version     Just print the current firmware version"
+		echo " -l, --latest      Use latest repo version (instead of stable version)"
+		echo " -f, --force       Force the upgrade, regardless of versions"
+		echo " -c, --check       Only compare versions, do not actually update"
+		echo " -u, --ubus        Script outputs only json"
 
-    echo ""
+		echo ""
 }
 
 # print to stdout if not doing json output
@@ -135,20 +136,20 @@ GetRepoVersion () {
 	if [ $? -eq 0 ]; then
 		# parse the json file
 		local RESP="$(cat $outFile)"
-    # echo "response is $RESP"
+		# echo "response is $RESP"
 
 		json_load "$RESP"
 
 		# check the json file contents
 		json_get_var repoVersion version
-    # echo "repoVersion $repoVersion"
+		# echo "repoVersion $repoVersion"
 
 		repoVersionMajor=$(GetVersionMajor "$repoVersion")
 		repoVersionMinor=$(GetVersionMinor "$repoVersion")
 		repoVersionRev=$(GetVersionRevision "$repoVersion")
 
 		json_get_var repoBuildNum build
-    # echo "repoBuildNum $repoBuildNum"
+		# echo "repoBuildNum $repoBuildNum"
 
 		# parse the binary url
 		json_get_var repoBinary url
@@ -158,9 +159,9 @@ GetRepoVersion () {
 
 		fileSize=$(GetFileSize "$repoBinary")
 
-    # echo "repoVersionMajor $repoVersionMajor"
-    # echo "repoVersionMinor $repoVersionMinor"
-    # echo "repoVersionRev $repoVersionRev"
+		# echo "repoVersionMajor $repoVersionMajor"
+		# echo "repoVersionMinor $repoVersionMinor"
+		# echo "repoVersionRev $repoVersionRev"
 	fi
 	# echo "$ret"
 }
@@ -178,6 +179,59 @@ JsonAddVersion () {
 	json_close_object
 }
 
+# function to compare versions and determine if upgrade is necessary
+# arguments
+#		new version number - major number 
+#		new version number - minor number 
+#		new version number - revision number 
+#		old version number - major number 
+#		old version number - minor number 
+#		old version number - revision number 
+VersionNumberCompare () {
+	local newVersionMajor=$1
+	local newVersionMinor=$2
+	local newVersionRev=$3
+	
+	local oldVersionMajor=$4
+	local oldVersionMinor=$5
+	local oldVersionRev=$6
+	
+	local bUpgradeReq=0
+	
+	if 	[ $newVersionMajor -gt $oldVersionMajor ]
+	then
+		bUpgradeReq=1
+	elif 	[ $newVersionMajor -eq $oldVersionMajor ] &&
+			[ $newVersionMinor -gt $oldVersionMinor ];
+	then
+		bUpgradeReq=1
+	elif 	[ $newVersionMajor -eq $oldVersionMajor ] &&
+			[ $newVersionMinor -eq $oldVersionMinor ] &&
+			[ $newVersionRev -gt $oldVersionRev ];
+	then
+		bUpgradeReq=1
+	fi
+	
+	echo $bUpgradeReq
+}
+
+# function to compare build numbers and determine if upgrade is necessary
+# arguments
+#		new build number
+# 	old build number 
+BuildNumberCompare () {
+	local newBuildNumber=$1
+	local oldBuildNumber=$2
+	local newVersionRev=$3
+	
+	local bBuildsDiff=0
+	
+	if [ $newBuildNumber -gt $oldBuildNumber ]; then
+		bBuildsDiff=1
+	fi
+
+	echo $bBuildsDiff
+}
 
 
 ########################
@@ -186,35 +240,38 @@ JsonAddVersion () {
 # read arguments
 while [ "$1" != "" ]
 do
-    case "$1" in
-        -h|-help|--help)
-            bUsage=1
-        ;;
-        -v|-version|--version)
-            bDeviceVersion=1
-            bRepoVersion=0
-        ;;
-        -f|-force|--force)
-            bCheck=0
-            bUpgrade=1
-        ;;
-        -c|-check|--check)
-            bCheckOnly=1
-        ;;
-        -l|-latest|--latest)
-            bLatest=1
-        ;;
-        -u|-ubus)
-            bJsonOutput=1
-        ;;
-        *)
-            echo "ERROR: Invalid Argument"
-            echo ""
-            bUsage=1
-        ;;
-    esac
+		case "$1" in
+				-h|-help|--help)
+						bUsage=1
+				;;
+				-v|-version|--version)
+						bDeviceVersion=1
+						bRepoVersion=0
+				;;
+				-f|-force|--force)
+						bCheck=0
+						bUpgrade=1
+				;;
+				-c|-check|--check)
+						bCheckOnly=1
+				;;
+				-l|-latest|--latest)
+						bLatest=1
+				;;
+				-u|-ubus|-j|-json|--json)
+						bJsonOutput=1
+				;;
+				-d|-debug|--debug)
+						bDebug=1
+				;;
+				*)
+						echo "ERROR: Invalid Argument"
+						echo ""
+						bUsage=1
+				;;
+		esac
 
-    shift
+		shift
 done
 
 
@@ -250,7 +307,7 @@ if [ $bRepoVersion == 1 ]; then
 	Print "url: $repoFile"
 
 	# fetch the repo version
-  GetRepoVersion $repoFile
+	GetRepoVersion $repoFile
 
 	if [ "$repoVersion" == "" ]; then
 		Print "> ERROR: Could not connect to Onion Firmware Server!"
@@ -274,29 +331,14 @@ fi
 if 	[ $bCheck == 1 ]
 then
 	Print "> Comparing version numbers"
-
-	if 	[ $repoVersionMajor -gt $deviceVersionMajor ]
-	then
-		bUpgrade=1
-	elif 	[ $repoVersionMajor -eq $deviceVersionMajor ] &&
-		 	[ $repoVersionMinor -gt $deviceVersionMinor ];
-	then
-		bUpgrade=1
-	elif 	[ $repoVersionMajor -eq $deviceVersionMajor ] &&
-		 	[ $repoVersionMinor -eq $deviceVersionMinor ] &&
-		 	[ $repoVersionRev -gt $deviceVersionRev ];
-	then
-		bUpgrade=1
-	fi
+  bUpgrade=$(VersionNumberCompare $repoVersionMajor $repoVersionMinor $repoVersionRev $deviceVersionMajor $deviceVersionMinor $deviceVersionRev)
 fi
 
 
 ## compare the build numbers (only if versions are the same)
 if 	[ $bUpgrade == 0 ]
 then
-	if [ $repoBuildNum -gt $deviceBuildNum ]; then
-		bBuildMismatch=1
-	fi
+  bBuildMismatch=$(BuildNumberCompare $repoBuildNum $deviceBuildNum)
 fi
 
 
@@ -371,7 +413,9 @@ then
 		sleep 5 	# wait 5 seconds before starting the firmware upgrade
 		Print "> Starting firmware upgrade...."
 
-		sysupgrade $localBinary
+		if [ $bDebug == 0	 ]; then
+			sysupgrade $localBinary
+		fi
 	else
 		Print "> ERROR: Downloading firmware has failed! Try again!"
 	fi
